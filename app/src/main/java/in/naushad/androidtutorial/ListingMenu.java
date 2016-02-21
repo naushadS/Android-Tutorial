9ddf52ee73efb6037d7b747a36902d0ece23e847
Naushad Shukoor <naushadshukoor@gmail.com>
2016-02-21 22:00:47 +0530

    Minor UI changes

--- app/src/main/java/in/naushad/androidtutorial/ListingMenu.java
+++ app/src/main/java/in/naushad/androidtutorial/ListingMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,136 +28,161 @@ public class ListingMenu extends AppCompatActivity {
 
 
     public String classes[] = {"Incrementor/Decrementor", "Android Police (Web View)", "Android Police (Chrome Custom Tab)",
-            "Text Play", "Image Capture","Change Layout Background","Picasso Library","Email the Developer!",
-            "example9","example10","example11","example12","example13",
-            "example14","example15","example16","example17","example18",
-            "example19","example20"};
+            "Text Play", "Image Capture", "Change Layout Background", "Picasso Library", "Email the Developer!",
+            "example9", "example10", "example11", "example12", "example13",
+            "example14", "example15", "example16", "example17", "example18",
+            "example19", "example20", "example21", "example22", "example23", "example24", "example25",
+            "example26", "example27", "example28", "example29", "example30",
+            "example31", "example32"};
     ListView lvListingMenu;
-    private Intent drawerintent=null;
-    private Drawer result=null;
-    private AccountHeader headerResult=null;
+    Toolbar tbListingMenu;
+    private Intent drawerintent = null;
+    private Drawer result = null;
+    private AccountHeader headerResult = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing_menu);
 
-        //Account Header for Material Drawer
-        headerResult = new AccountHeaderBuilder()
-                .withActivity(this)
-                .withHeaderBackground(R.drawable.header)
-                .addProfiles(
-                        new ProfileDrawerItem()
-                                .withName("Naushad Shukoor")
-                                .withEmail("naushadshukoor@gmail.com")
-                                .withIcon(getResources().getDrawable(R.drawable.profile3))
-                )
-                .addProfiles(
-                        new ProfileDrawerItem()
-                                .withName("Bruce Wayne")
-                                .withEmail("brucewayne@gmail.com")
-                                .withIcon(getResources().getDrawable(R.drawable.profile3))
-                )
-                .build();
 
-         //Material Drawer for listing menu activity
-                 result = new DrawerBuilder()
-                .withActivity(this)
-                .withTranslucentStatusBar(false)
-                .withTranslucentNavigationBar(false)
-                .withFullscreen(false)
-                .withSelectedItem(-1)
-                .withAccountHeader(headerResult)
-                .addDrawerItems(
-                        new PrimaryDrawerItem()
-                                .withIcon(R.drawable.email)
-                                .withName("Email the Developer!")
-                                .withDescription("Send a pre-formatted mail")
-                                .withBadge("1")
-                                .withBadgeStyle(new BadgeStyle().withTextColor(Color.WHITE).withColorRes(R.color.Black))
-                                .withIdentifier(1)
-                                .withSelectable(false)
+            tbListingMenu = (Toolbar) findViewById(R.id.tbListingMenu);
+            setSupportActionBar(tbListingMenu);
+            getSupportActionBar().setTitle("List Of Options");
+            getSupportActionBar().setSubtitle("Select One Already !");
 
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        if (drawerItem != null) {
-                            drawerintent = null;
+        /*
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+ */
+            //show back arrow
+            //result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(false);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            //show hamburger icon
+           //getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            //result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+
+
+            //Account Header for Material Drawer
+            headerResult = new AccountHeaderBuilder()
+                    .withActivity(this)
+                    .withHeaderBackground(R.drawable.header)
+                    .addProfiles(
+                            new ProfileDrawerItem()
+                                    .withName("Naushad Shukoor")
+                                    .withEmail("naushadshukoor@gmail.com")
+                                    .withIcon(getResources().getDrawable(R.drawable.profile3))
+                    )
+                    .addProfiles(
+                            new ProfileDrawerItem()
+                                    .withName("Bruce Wayne")
+                                    .withEmail("brucewayne@gmail.com")
+                                    .withIcon(getResources().getDrawable(R.drawable.profile3))
+                    )
+                    .build();
+
+            //Material Drawer for listing menu activity
+            result = new DrawerBuilder()
+                    .withActivity(this)
+                    .withToolbar(tbListingMenu)
+                    .withTranslucentStatusBar(true)
+                    .withTranslucentNavigationBar(false)
+                    .withFullscreen(false)
+                    .withSelectedItem(-1)
+                    .withActionBarDrawerToggle(true)
+                    .withAccountHeader(headerResult)
+                    .addDrawerItems(
+                            new PrimaryDrawerItem()
+                                    .withIcon(R.drawable.email)
+                                    .withName("Email the Developer!")
+                                    .withDescription("Send a pre-formatted mail")
+                                    .withBadge("1")
+                                    .withBadgeStyle(new BadgeStyle().withTextColor(Color.WHITE).withColorRes(R.color.Black))
+                                    .withIdentifier(1)
+                                    .withSelectable(false)
+
+                    )
+                    .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                        @Override
+                        public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                            if (drawerItem != null) {
+                                drawerintent = null;
+                            }
+                            if (drawerItem.getIdentifier() == 1) {
+                                drawerintent = new Intent(ListingMenu.this, Email_Dev.class);
+                            }
+                            if (drawerintent != null) {
+                                startActivity(drawerintent);
+                            }
+                            return false;
                         }
-                        if (drawerItem.getIdentifier() == 1) {
-                            drawerintent = new Intent(ListingMenu.this, Email_Dev.class);
-                        }
-                        if (drawerintent != null) {
-                            startActivity(drawerintent);
-                        }
-                        return false;
+                    })
+                    .build();
+
+
+            // Loading the banner ad in listing menu
+            AdView avListingMenu = (AdView) findViewById(R.id.avListingMenu);
+            AdRequest adRequestListingMenu = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
+                    .addTestDevice("A851D03B6D976CAA2BDAABFC232841DC")  // My Xiaomi Redmi 1s test phone
+                    .build();
+            avListingMenu.loadAd(adRequestListingMenu);
+
+
+            // Populating List View
+            ArrayAdapter<String> naushadAdapter = new ArrayAdapter<String>(ListingMenu.this, android.R.layout.simple_list_item_1, classes);
+            lvListingMenu = (ListView) findViewById(R.id.lvListingMenu);
+            lvListingMenu.setAdapter(naushadAdapter);
+
+
+            // OnItemClickListener for List Items
+            lvListingMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String cheese = classes[position];
+
+                    //converting display name to class name
+                    switch (cheese) {
+                        case "Incrementor/Decrementor":
+                            cheese = "startingPoint";
+                            break;
+                        case "Android Police (Web View)":
+                            cheese = "webView";
+                            break;
+                        case "Android Police (Chrome Custom Tab)":
+                            cheese = "chromeCustomTabActivity";
+                            break;
+                        case "Text Play":
+                            cheese = "TextPlay";
+                            break;
+                        case "Image Capture":
+                            cheese = "camera";
+                            break;
+                        case "Change Layout Background":
+                            cheese = "changeBackground";
+                            break;
+                        case "Picasso Library":
+                            cheese = "picassoLibrary";
+                            break;
+                        case "Email the Developer!":
+                            cheese = "Email_Dev";
+                            break;
                     }
-                })
-                         .build();
 
-
-                        // Loading the banner ad in listing menu
-                        AdView avListingMenu = (AdView) findViewById(R.id.avListingMenu);
-                        AdRequest adRequestListingMenu = new AdRequest.Builder()
-                                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)        // All emulators
-                                .addTestDevice("A851D03B6D976CAA2BDAABFC232841DC")  // My Xiaomi Redmi 1s test phone
-                                .build();
-                        avListingMenu.loadAd(adRequestListingMenu);
-
-
-                        // Populating List View
-                        ArrayAdapter<String> naushadAdapter = new ArrayAdapter<String>(ListingMenu.this, android.R.layout.simple_list_item_1, classes);
-                        lvListingMenu = (ListView) findViewById(R.id.lvListingMenu);
-                        lvListingMenu.setAdapter(naushadAdapter);
-
-
-                        // OnItemClickListener for List Items
-                        lvListingMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                String cheese = classes[position];
-
-                                //converting display name to class name
-                                switch (cheese) {
-                                    case "Incrementor/Decrementor":
-                                        cheese = "startingPoint";
-                                        break;
-                                    case "Android Police (Web View)":
-                                        cheese = "webView";
-                                        break;
-                                    case "Android Police (Chrome Custom Tab)":
-                                        cheese = "chromeCustomTabActivity";
-                                        break;
-                                    case "Text Play":
-                                        cheese = "TextPlay";
-                                        break;
-                                    case "Image Capture":
-                                        cheese = "camera";
-                                        break;
-                                    case "Change Layout Background":
-                                        cheese = "changeBackground";
-                                        break;
-                                    case "Picasso Library":
-                                        cheese = "picassoLibrary";
-                                        break;
-                                    case "Email the Developer!":
-                                        cheese = "Email_Dev";
-                                        break;
-                                }
-
-                                //redirecting the registered click by user to the appropriate class
-                                Class ourClass = null;
-                                try {
-                                    ourClass = Class.forName("in.naushad.androidtutorial." + cheese);
-                                    Intent ourIntent = new Intent(ListingMenu.this, ourClass);
-                                    startActivity(ourIntent);
-                                } catch(ClassNotFoundException e) {
-                    e.printStackTrace();
+                    //redirecting the registered click by user to the appropriate class
+                    Class ourClass = null;
+                    try {
+                        ourClass = Class.forName("in.naushad.androidtutorial." + cheese);
+                        Intent ourIntent = new Intent(ListingMenu.this, ourClass);
+                        startActivity(ourIntent);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+
 
     @Override
     public void onBackPressed() {
@@ -181,14 +207,18 @@ public class ListingMenu extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_email_dev) {
-            Intent emaildevintent = new Intent(ListingMenu.this,Email_Dev.class);
-            startActivity(emaildevintent);
-            return true;
+            case R.id.action_email_dev:
+                Intent emaildevintent = new Intent(ListingMenu.this, Email_Dev.class);
+                startActivity(emaildevintent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/in/naushad/androidtutorial/picassoLibrary.java
+++ app/src/main/java/in/naushad/androidtutorial/picassoLibrary.java
@@ -1,19 +1,31 @@
 package in.naushad.androidtutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-public class picassoLibrary extends AppCompatActivity {
+public class picassoLibrary extends AppCompatActivity{
     ImageView ivPicasso1;
+    TextView tvPicassoDesc;
+    Toolbar tbPicasso;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picasso_library);
+        tbPicasso = (Toolbar) findViewById(R.id.tbPicasso);
+        tvPicassoDesc=(TextView)findViewById(R.id.tvPicassoDesc);
+        setSupportActionBar(tbPicasso);
+        getSupportActionBar().setTitle("Picasso (Library)");
+        getSupportActionBar().setSubtitle("Image Loading & Caching");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
 
         ivPicasso1 = (ImageView) findViewById(R.id.ivPicasso1);
 
@@ -26,8 +38,27 @@ public class picassoLibrary extends AppCompatActivity {
                 //.placeholder(R.drawable.splash_background)
                 //.error(R.drawable.splash_background)
                 .into(ivPicasso1);
+        tvPicassoDesc.append("\n"
+                +"Red = Image Source(Network)"
+                +"\n"
+                +"Blue = Image Source(Disk)"
+                +"\n"
+                +"Green = Image Source(Cache)");
+
 
 
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle the click on the back arrow click
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
